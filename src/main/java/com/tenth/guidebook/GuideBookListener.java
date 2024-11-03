9b88776c934097edca233ec2c6f4ddbdc87a75d2
tenthdragon103 <115429203+tenthdragon103@users.noreply.github.com>
2024-11-02 20:31:19 -0700

    init

---
+++ src/main/java/com/tenth/guidebook/GuideBookListener.java
@@ -0,0 +1,15 @@
+package com.tenth.guidebook;
+
+import org.bukkit.event.Listener;
+
+public class GuideBookListener implements Listener {
+
+    private final GuideBook plugin;
+
+    public GuideBookListener(GuideBook plugin) {
+        this.plugin = plugin;
+    }
+
+    //over ride stuff? idk if this class is actually required or not
+
+}
